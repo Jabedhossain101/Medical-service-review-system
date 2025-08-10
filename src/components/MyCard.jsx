85ba15228f915f4637891a43935b11bb35c1a004
rafsan102 <ahmedrafsan101@gmail.com>
2025-08-10 16:27:04 +0600

    edit myservice page color and text

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -37,23 +37,23 @@ const MyCard = ({ service }) => {
     });
   };
   return (
-    <div className="max-w-7xl mx-auto px-4 py-6">
+    <div className="">
       <div className="h-20"></div>
       <div className="flex justify-center ">
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-xl bg-gradient-to-br from-[#eef1e1] to-[#e7efd7] relative p-4">
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-xl bg-[#FFFFF0] relative p-4">
           <img
             className="rounded-xl h-48 w-full object-cover"
             src={serviceImage} // Replace with your image
             alt="Istanbul"
           />
 
-          <div className="bg-[#415411] text-white italic text-lg font-semibold px-3 py-1 mt-2 inline-block rounded-md shadow-md">
+          <div className=" text-gry-800 italic text-lg font-semibold px-3 py-1 mt-2 inline-block rounded-md shadow-md">
             {companyName}
           </div>
 
-          <div className="mt-3 text-[#2e3b21] text-sm space-y-1">
+          <div className="mt-3 text-sm space-y-1">
             <p>
-              <span className="text-[#748c38] font-semibold">Category:</span>{' '}
+              <span className="text-gray-800 font-semibold">Category:</span>{' '}
               {category}
             </p>
             <p>
@@ -67,20 +67,20 @@ const MyCard = ({ service }) => {
             <p>{serviceTitle}</p>
             <Link
               to={`/service/${_id}`}
-              className="bg-[#4b5f20] hover:bg-[#374818] btn text-white px-4 py-2 rounded-full flex items-center gap-1"
+              className="bg-[#ff9b00] hover:bg-[#ff9b04] btn text-white px-4 py-2 rounded flex items-center gap-1"
             >
               👁️ <span className="hidden sm:inline">View more</span>
             </Link>
           </div>
 
           <div className="flex flex-wrap gap-2 mt-4">
-            <span className="bg-[#e4eac5] text-[#2c3d1f] px-3 py-1 rounded-full text-sm font-medium shadow-sm">
+            <span className="bg-[#FFFFF0] text-[#000] px-3 py-1 rounded-full text-sm font-medium shadow-sm">
               Medical instrument
             </span>
-            <span className="bg-[#e4eac5] text-[#2c3d1f] px-3 py-1 rounded-full text-sm font-medium shadow-sm">
+            <span className="bg-[#FFFFF0] text-[#000] px-3 py-1 rounded-full text-sm font-medium shadow-sm">
               Operation theatre
             </span>
-            <span className="bg-[#e4eac5] text-[#2c3d1f] px-3 py-1 rounded-full text-sm font-medium shadow-sm">
+            <span className="bg-[#FFFFF0] text-[#000] px-3 py-1 rounded-full text-sm font-medium shadow-sm">
               Time to time
             </span>
           </div>
@@ -88,20 +88,20 @@ const MyCard = ({ service }) => {
           <div className="flex justify-between items-center mt-6 px-2 flex-wrap gap-2">
             <Link
               to={'/addService'}
-              className="bg-[#4b5f20] hover:bg-[#374818] text-white px-4 py-2 rounded-full flex items-center gap-1"
+              className="bg-[#FFFFF0] btn text-gray-800 px-4 py-2 rounded flex items-center gap-1 hover:bg-base-200 transition-colors duration-300"
             >
               ➕ <span className="hidden sm:inline">Add</span>
             </Link>
             <Link
               to={`/update/${_id}`}
-              className="bg-[#4b5f20] hover:bg-[#374818] text-white px-4 py-2 rounded-full flex items-center gap-1"
+              className="bg-[#FFFFF0] btn text-gray-800 px-4 py-2 rounded flex items-center gap-1 hover:bg-base-200 transition-colors duration-300"
             >
               ✏️ <span className="hidden sm:inline">Update</span>
             </Link>
 
             <button
               onClick={() => handleDelete(_id)}
-              className="bg-[#4b5f20] hover:bg-[#374818] text-white px-4 py-2 rounded-full flex items-center gap-1"
+              className="bg-[#FFFFF0] btn text-gray-800 px-4 py-2 rounded flex items-center gap-1 hover:bg-base-200 transition-colors duration-300"
             >
               🗑️ <span className="hidden sm:inline">Delete</span>
             </button>
